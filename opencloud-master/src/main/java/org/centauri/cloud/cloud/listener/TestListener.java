14417369a33a3e0c8592b43ceb770cb703be8f23
Fxshlein <Neonfxsh@gmail.com>
2017-06-12 17:35:48 +0200

    Fancier start message!

--- opencloud-master/src/main/java/org/centauri/cloud/cloud/listener/TestListener.java
+++ opencloud-master/src/main/java/org/centauri/cloud/cloud/listener/TestListener.java
@@ -1,6 +1,5 @@
 package org.centauri.cloud.cloud.listener;
 
-import org.centauri.cloud.cloud.Cloud;
 import org.centauri.cloud.cloud.event.Listener;
 import org.centauri.cloud.cloud.event.events.ConsoleCommandEvent;
 
