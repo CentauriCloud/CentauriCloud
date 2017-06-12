25a68f815c9f76f6721cdd8c8c6806ffc6cad522
Fxshlein <Neonfxsh@gmail.com>
2017-06-12 20:00:40 +0200

    Adds Netty implementation to Bungee

---
+++ opencloud-bungee-connector/src/main/java/org/centauri/cloud/bungee/netty/NetworkHandler.java
@@ -0,0 +1,18 @@
+package org.centauri.cloud.bungee.netty;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import org.centauri.cloud.cloud.network.packets.Packet;
+import org.centauri.cloud.cloud.network.packets.PacketPing;
+
+
+public class NetworkHandler extends SimpleChannelInboundHandler<Packet> {
+	
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, Packet packet) throws Exception {
+		if(packet instanceof PacketPing) {
+			ctx.channel().writeAndFlush(packet);
+		}
+	}
+
+}
