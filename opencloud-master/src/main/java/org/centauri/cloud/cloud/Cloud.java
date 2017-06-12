7cf2d40f129eab7cde0c14f142fbb1b9e07832ad
Microsamp <germancraft42@gmail.com>
2017-06-12 14:13:34 +0200

    Start server

--- opencloud-master/src/main/java/org/centauri/cloud/cloud/Cloud.java
+++ opencloud-master/src/main/java/org/centauri/cloud/cloud/Cloud.java
@@ -6,12 +6,14 @@ import org.centauri.cloud.cloud.config.PropertyManager;
 import org.centauri.cloud.cloud.event.EventManager;
 import org.centauri.cloud.cloud.io.Console;
 import org.centauri.cloud.cloud.listener.TestListener;
+import org.centauri.cloud.cloud.network.Server;
 
 public class Cloud {
 
 	@Getter private static Cloud instance;
 	@Getter @Setter private boolean running;
 	@Getter private EventManager eventManager;
+	@Getter private Server server;
 	
 	public Cloud() {
 		instance = this;
@@ -21,11 +23,18 @@ public class Cloud {
 		PropertyManager manager = new PropertyManager();
 		manager.load();
 		
+		this.eventManager = new EventManager();
+		
 		ModuleLoader loader = new ModuleLoader();
 		loader.initializeScheduler();
 		
-		this.eventManager = new EventManager();
-		
+		this.server = new Server();
+		try{
+			server.run(Integer.valueOf(manager.getProperties().getProperty("port")));
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			this.running = false;//Stop server
+		}
 		this.registerListeners();
 		
 		System.out.println("Cloud started");
