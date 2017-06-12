840f30952911c3cf8aa2ffddf302926a5ac3def9
Microsamp <germancraft42@gmail.com>
2017-06-12 12:16:59 +0200

    Clean up NetworkHandler in master

--- opencloud-master/src/main/java/org/centauri/cloud/cloud/network/NetworkHandler.java
+++ opencloud-master/src/main/java/org/centauri/cloud/cloud/network/NetworkHandler.java
@@ -1,25 +1,13 @@
 package org.centauri.cloud.cloud.network;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import org.centauri.cloud.cloud.network.packets.Packet;
 
-public class NetworkHandler extends SimpleChannelInboundHandler<ByteBuf> {
-
-	private Channel channel;
-
+public class NetworkHandler extends SimpleChannelInboundHandler<Packet> {
 
 	@Override
-	public void channelActive(ChannelHandlerContext ctx) throws Exception {
-		this.channel = ctx.channel();
-	}
-
-	@Override
-	protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
-		switch (byteBuf.readByte()) {
-
-
-		}
+	protected void channelRead0(ChannelHandlerContext channelHandlerContext, Packet packet) throws Exception {
+		
 	}
 }
