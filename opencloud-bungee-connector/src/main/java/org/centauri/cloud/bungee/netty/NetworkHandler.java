d6db5fac6b4072ee2df1d90ee0af85210e01ce8b
Microsamp <germancraft42@gmail.com>
2017-06-13 17:32:34 +0200

    Register server in bungee

--- opencloud-bungee-connector/src/main/java/org/centauri/cloud/bungee/netty/NetworkHandler.java
+++ opencloud-bungee-connector/src/main/java/org/centauri/cloud/bungee/netty/NetworkHandler.java
@@ -2,7 +2,10 @@ package org.centauri.cloud.bungee.netty;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import java.net.InetSocketAddress;
+import org.centauri.cloud.bungee.util.ServerUtil;
 import org.centauri.cloud.cloud.network.packets.Packet;
+import org.centauri.cloud.cloud.network.packets.PacketBungeeRegisterServer;
 import org.centauri.cloud.cloud.network.packets.PacketPing;
 
 
@@ -12,6 +15,9 @@ public class NetworkHandler extends SimpleChannelInboundHandler<Packet> {
 	protected void channelRead0(ChannelHandlerContext ctx, Packet packet) throws Exception {
 		if(packet instanceof PacketPing) {
 			ctx.channel().writeAndFlush(packet);
+		} else if(packet instanceof PacketBungeeRegisterServer) {
+			PacketBungeeRegisterServer registerServer = (PacketBungeeRegisterServer) packet;
+			ServerUtil.addServer(registerServer.getName(), new InetSocketAddress(registerServer.getHost(), registerServer.getBukkitPort()), "CentauriCloud hosted server", true);
 		}
 	}
 
