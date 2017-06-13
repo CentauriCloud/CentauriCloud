fd02b9a39df7435b1ca95999d08ddbc838f5cb97
Microsamp <germancraft42@gmail.com>
2017-06-13 15:10:32 +0200

    Implemented help screen

--- opencloud-master/src/main/java/org/centauri/cloud/cloud/listener/OpenCloudCommandListener.java
+++ opencloud-master/src/main/java/org/centauri/cloud/cloud/listener/OpenCloudCommandListener.java
@@ -14,26 +14,45 @@ public class OpenCloudCommandListener {
 				Cloud.getInstance().stop();
 				break;
 			case "info":
-				System.out.println("OpenCloud v1.0 developed by Centauri Developer Team");
-				System.out.println("Centauri Developer Team: ");
-				System.out.println("Founder: Microsamp(Steve) & Fxshlein(Liam) & byImmortal(Joel)");
-				System.out.println("Developer: MoVo99(Moritz) & Tobi14601(Tobi)");
-				System.out.println("Contributors: -");
+				this.displayInfo();
+				break;
+			case "help":
+				this.displayHelp();
 				break;
 			case "pl":
 			case "plugins":
-				final StringBuilder sb = new StringBuilder();
-				sb.append("Plugins: \n");
-				Cloud.getInstance().getModuleManager().getLoaded().forEach(pl -> {
-					sb.append(pl);
-					sb.append(", ");
-				});
-				System.out.println(sb.toString());
+				this.displayPlugins();
 				break;
 			default:
-				System.out.println("Cannot find command: " + input);
+				System.out.println("Cannot find command: " + input + "\n Type \"help\" for help");
 				break;
 		}
+	}
+	
+	private void displayHelp() {
+		System.out.println("Basic commands: \n"
+				+ "help - this help screen\n"
+				+ "plugins(pl) - display all plugins\n"
+				+ "info - display informations about openCloud and the team\n"
+				+ "stop - stop the master");
+	}
+	
+	private void displayPlugins() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("Plugins: \n");
+		Cloud.getInstance().getModuleManager().getLoaded().forEach(pl -> {
+			sb.append(pl);
+			sb.append(", ");
+		});
+		System.out.println(sb.toString());
+	}
+	
+	private void displayInfo() {
+		System.out.println("OpenCloud v1.0 developed by Centauri Developer Team");
+		System.out.println("Centauri Developer Team: ");
+		System.out.println("Founder: Microsamp(Steve) & Fxshlein(Liam) & byImmortal(Joel)");
+		System.out.println("Developer: MoVo99(Moritz) & Tobi14601(Tobi)");
+		System.out.println("Contributors: -");
 	}
 	
 }
