563a39cab9aa2949a178fd626cd8be819b207ca3
Microsamp <germancraft42@gmail.com>
2017-06-14 06:32:01 +0200

    Log server registering

--- centauricloud-bungee-connector/src/main/java/org/centauri/cloud/bungee/netty/NetworkHandler.java
+++ centauricloud-bungee-connector/src/main/java/org/centauri/cloud/bungee/netty/NetworkHandler.java
@@ -21,6 +21,7 @@ public class NetworkHandler extends SimpleChannelInboundHandler<Packet> {
 		} else if(packet instanceof PacketBungeeRegisterServer) {
 			PacketBungeeRegisterServer registerServer = (PacketBungeeRegisterServer) packet;
 			ServerUtil.addServer(registerServer.getName(), new InetSocketAddress(registerServer.getHost(), registerServer.getBukkitPort()), "CentauriCloud hosted server", true);
+			BungeeConnectorPlugin.getPluginLogger().info("Registered server: " + registerServer.getName());
 		}
 	}
 	
