462f7caf5d08ae7493a8761ca4fed4e4e6fad84c
LordOtut <pascal99.schmidt@googlemail.com>
2017-06-15 19:05:02 +0200

    added ConfigUpdater & little bugfix

---
+++ centauricloud-common/src/main/java/org/centauri/cloud/common/network/util/ConfigUpdater.java
@@ -0,0 +1,89 @@
+package org.centauri.cloud.common.network.util;
+
+import lombok.Getter;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConfigUpdater {
+
+    @Getter private static ConfigUpdater instance;
+
+    public ConfigUpdater() {
+        instance = this;
+    }
+
+    public void updateConfig(File currentfile, String defaultfilepath) {
+
+        try {
+
+            List<String> defaultLines = new ArrayList<>();
+            List<String> currentLines = new ArrayList<>();
+
+            InputStream in = getClass().getResourceAsStream(defaultfilepath);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            try {
+                for (String line; (line = reader.readLine()) != null; ) {
+                    defaultLines.add(line);
+                }
+            } finally {
+                reader.close();
+            }
+
+            reader = new BufferedReader(new FileReader(currentfile));
+            try {
+                for (String line; (line = reader.readLine()) != null; ) {
+                    currentLines.add(line);
+                }
+            } finally {
+                reader.close();
+            }
+
+            for(int i = 0; i < defaultLines.size(); i++) {
+                String defaultLine = defaultLines.get(i);
+                String currentLine;
+                try {
+                    currentLine = currentLines.get(i);
+                } catch (Exception e) {
+                    currentLine = null;
+                }
+                if(currentLine != null) {
+                    if(!currentLine.startsWith("#") && currentLine.contains("=")) {
+                        String currentKey = currentLine.split("=")[0];
+                        String defaultKey = defaultLine.split("=")[0];
+                        if(!currentKey.equals(defaultKey)) {
+                            currentLines.set(i, defaultLine);
+                        }
+                    } else {
+                        currentLines.set(i, defaultLine);
+                    }
+                } else {
+                    currentLines.add(defaultLine);
+                }
+            }
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(currentfile));
+            try {
+                for (String lineToWrite : currentLines) {
+                    writer.write(lineToWrite + "\n");
+                }
+            } finally {
+                writer.close();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
