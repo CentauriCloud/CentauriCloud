67ff516116ee2e06613ed43f9f69f38a3355c033
LordOtut <pascal99.schmidt@googlemail.com>
2017-06-18 02:48:48 +0200

    added API methods

--- centauricloud-master/src/main/java/org/centauri/cloud/cloud/api/Centauri.java
+++ centauricloud-master/src/main/java/org/centauri/cloud/cloud/api/Centauri.java
@@ -1,9 +1,15 @@
 package org.centauri.cloud.cloud.api;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
 import lombok.Getter;
 import org.centauri.cloud.cloud.Cloud;
 import org.centauri.cloud.cloud.api.manager.LibraryManager;
@@ -48,12 +54,83 @@ public class Centauri {
 
 	public boolean sendCommandToServer(String cmd, String servername) {
 		Server server = this.getServer(servername);
-		if (server == null) {
+		if (server == null || cmd.isEmpty()) {
 			return false;
 		}
 		server.sendPacket(new PacketToServerDispatchCommand(cmd));
 		return true;
 	}
+
+	public String getCloudVersion() {
+		return "1.0";
+	}
+
+	public List<String> getConfigFromTemplate(String templatename) {
+        try {
+            Template template = Cloud.getInstance().getTemplateManager().getTemplate(templatename);
+            if(template == null) {
+                return null;
+            }
+            BufferedReader reader = new BufferedReader(new FileReader(template.getConfig()));
+            List<String> lines;
+            try {
+                lines = reader.lines().collect(Collectors.toList());
+            } finally {
+                reader.close();
+            }
+            return lines;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<String> getConfigFromServer(String servername) {
+        Server server = this.getServer(servername);
+        //TODO
+        return null;
+    }
+
+	public List<String> getFileContent(String path) {
+        try {
+            File file = new File(path);
+            if(!file.exists()) {
+                return null;
+            }
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            List<String> lines;
+            try {
+                lines = reader.lines().collect(Collectors.toList());
+            } finally {
+                reader.close();
+            }
+            return lines;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean setFileContent(String path, List<String> content) {
+        try {
+            File file = new File(path);
+            if(!file.exists() || content.isEmpty()) {
+                return false;
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            try {
+                for (String aContent : content) {
+                    writer.write(aContent);
+                }
+            } finally {
+                writer.close();
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 	
 	public EventManager getEventManager() {
 		return Cloud.getInstance().getEventManager();
