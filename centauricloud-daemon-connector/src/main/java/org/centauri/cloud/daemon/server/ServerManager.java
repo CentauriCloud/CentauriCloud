102abc0f7dd9f28c9083aa2a1ad5c250e0ca1251
jsauvain <joel.sauvain@hotmail.com>
2017-06-19 16:23:11 +0200

    fixed bug with port delivered multiple times

--- centauricloud-daemon-connector/src/main/java/org/centauri/cloud/daemon/server/ServerManager.java
+++ centauricloud-daemon-connector/src/main/java/org/centauri/cloud/daemon/server/ServerManager.java
@@ -1,5 +1,9 @@
 package org.centauri.cloud.daemon.server;
 
+import lombok.Getter;
+import lombok.SneakyThrows;
+import org.centauri.cloud.common.network.util.PortChecker;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
@@ -7,45 +11,40 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
-import lombok.Getter;
-import lombok.SneakyThrows;
-import org.centauri.cloud.common.network.util.PortChecker;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ServerManager {
 
 	@Getter private Set<Template> templates = new HashSet<>();
 	private final List<Process> serverProcesses = new ArrayList<>();
-	
+
+	private AtomicInteger integer = new AtomicInteger(11 * 1000);
+
 	@SneakyThrows
 	public void startServer(String templateName) {
-		this.templates.forEach(template -> {
-			try {
-				if (template.getName().equals(templateName)) {
-					File copy = template.createCopy();
-					Properties config = new Properties();
-					config.load(new FileInputStream(copy.getPath() + "/centauricloud.properties"));
-					ProcessBuilder builder = new ProcessBuilder(
-							config.getProperty("startCommand").replaceAll("%port%", String.valueOf(this.getNextPort())).split(" ")
-					);
-					builder.directory(copy);
-					Process process = builder.start();
-					this.serverProcesses.add(process);
-					System.out.println("Started new server from template: " + templateName);
-				}
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		});
+		Template template = this.templates.stream().filter(tmp -> tmp.getName().equals(templateName))
+				.findAny().orElse(null);
+		if (template == null)
+			return;
+
+		File copy = template.createCopy();
+		Properties config = new Properties();
+		config.load(new FileInputStream(copy.getPath() + "/centauricloud.properties"));
+		ProcessBuilder builder = new ProcessBuilder(
+				config.getProperty("startCommand").replaceAll("%port%", String.valueOf(this.getNextPort())).split(" ")
+		);
+		builder.directory(copy);
+		Process process = builder.start();
+		this.serverProcesses.add(process);
+		System.out.println("Started new server from template: " + templateName);
 	}
-	
+
 	private int getNextPort() {
-		for(int i = 11 * 1000; i < 12 * 1000; i++) {
-			if(PortChecker.available(i)) {
-				return i;
-			}
-		}
-		
-		return -1;
+		int nextInt;
+		do {
+			nextInt = integer.getAndIncrement();
+		} while (!PortChecker.available(nextInt));
+		return nextInt;
 	}
-	
+
 }
