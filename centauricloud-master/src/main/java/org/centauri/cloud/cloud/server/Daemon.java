1a5908be42aae0b3817bb08eb638466a40329dd1
jsauvain <joel.sauvain@hotmail.com>
2017-06-24 23:23:22 +0200

    bug fixed

--- centauricloud-master/src/main/java/org/centauri/cloud/cloud/server/Daemon.java
+++ centauricloud-master/src/main/java/org/centauri/cloud/cloud/server/Daemon.java
@@ -1,44 +1,44 @@
 package org.centauri.cloud.cloud.server;
 
 import io.netty.channel.Channel;
-import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
 import org.centauri.cloud.cloud.Cloud;
-import org.centauri.cloud.cloud.config.PropertyManager;
+import org.centauri.cloud.cloud.template.Template;
 import org.centauri.cloud.common.network.packets.PacketStartServer;
 import org.centauri.cloud.common.network.packets.PacketTemplateData;
-import org.centauri.cloud.cloud.template.Template;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Daemon extends Server {
-	
+
 	@Getter @Setter private List<Server> servers = Collections.synchronizedList(new ArrayList<Server>());
-	
+
 	public Daemon(Channel channel) {
 		super(channel);
 		this.sendTemplates();
 	}
-	
+
 	private void sendTemplates() {
 		Cloud.getInstance().getTemplateManager().getTemplates().forEach(template -> {
 			this.sendTemplate(template);
 		});
 	}
-	
+
 	@SneakyThrows
 	private void sendTemplate(Template template) {
-		FileInputStream inputStream = new FileInputStream(Cloud.getInstance().getTmpDir());
+		FileInputStream inputStream = new FileInputStream(Cloud.getInstance().getTmpDir() + "/" + template.getName() + ".zip");
 		byte[] data = new byte[inputStream.available()];
 		inputStream.read(data);
 		this.getChannel().writeAndFlush(new PacketTemplateData(template.getName(), data));
 	}
-	
+
 	public void startServer(String prefix) {
 		this.getChannel().writeAndFlush(new PacketStartServer(prefix));
 	}
-	
+
 }
