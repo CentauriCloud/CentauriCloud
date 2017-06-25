f9b19f7e795129bfc1ef0123b8e4dd9a9e0e3896
jsauvain <joel.sauvain@hotmail.com>
2017-06-25 02:02:17 +0200

    fromat

--- centauricloud-spigot-connector/src/main/java/org/centauri/cloud/spigot/config/CloudConfiguration.java
+++ centauricloud-spigot-connector/src/main/java/org/centauri/cloud/spigot/config/CloudConfiguration.java
@@ -1,21 +1,22 @@
 package org.centauri.cloud.spigot.config;
 
-import java.io.File;
-import java.util.logging.Level;
 import lombok.Getter;
 import org.centauri.cloud.common.network.config.TemplateConfig;
 import org.centauri.cloud.spigot.SpigotConnectorPlugin;
 
+import java.io.File;
+import java.util.logging.Level;
+
 public class CloudConfiguration {
-	
+
 	@Getter private String hostname;
 	@Getter private int port;
 	@Getter private String prefix;
-	
+
 	public CloudConfiguration(String path) {
 		try {
 			TemplateConfig config = new TemplateConfig(new File("."));
-			
+
 			this.hostname = config.getString("hostname");
 			this.port = config.getInt("port");
 			this.prefix = new File(".").getName();
