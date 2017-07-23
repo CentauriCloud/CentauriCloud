51244bdc67755779c52a652d2c55a25fb05c719e
Joel Sauvain <joel.sauvain@hotmail.com>
2017-07-23 20:48:43 +0200

    some refactor

--- centauricloud-master/src/main/java/org/centauri/cloud/cloud/download/ConnectorDownloader.java
+++ centauricloud-master/src/main/java/org/centauri/cloud/cloud/download/ConnectorDownloader.java
@@ -2,71 +2,21 @@ package org.centauri.cloud.cloud.download;
 
 import lombok.Getter;
 import org.centauri.cloud.cloud.Cloud;
-import org.centauri.cloud.cloud.util.ProgressUtil;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class ConnectorDownloader {
 
 	public void checkConnectorsAndDownload() {
 		for (ConnectorType type : ConnectorType.values()) {
 			if (!new File(Cloud.getInstance().getSharedDir(), type.getFile().getName()).exists()) {
-				download(type);
+				DownloadUtil.download(type.getRemotePath(), type.getFile());
 			}
-		}
-	}
-
-	private void download(ConnectorType type) {
-		try {
-			URL url = new URL(type.getRemotePath());
-			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-			urlConnection.setRequestMethod("HEAD");
-			urlConnection.getInputStream();
-			int allBytes = urlConnection.getContentLength();
-			urlConnection.disconnect();
-
-			File targetFile = new File(Cloud.getInstance().getSharedDir(), type.getFile().getCanonicalFile().getName());
-			if (!targetFile.exists())
-				targetFile.createNewFile();
-
-			try (InputStream fileInputStream = url.openStream();
-				 OutputStream outputStream = new FileOutputStream(targetFile)) {
-
-				int bufferSize = 4 * 1024;
-				byte[] buffer = new byte[bufferSize];
-				int count = 0;
-				int read;
-
-				System.out.println("Progress for downloading " + type.getFile().getName() + ":");
-				System.out.println("");
-
-				while ((read = fileInputStream.read(buffer)) != -1) {
-					outputStream.write(buffer, 0, read);
-					count += read;
-					ProgressUtil.printProgress(allBytes, count);
-				}
-
-				System.out.println("");
-				System.out.println("");
-
-			}
-
-		} catch (MalformedURLException e) {
-			Cloud.getLogger().error("Cannot create url", e);
-		} catch (IOException e) {
-			Cloud.getLogger().error("Cannot open stream, webserver down? Please contact CentauriTeam! Code: NO_CON");
 		}
 	}
 
 	public enum ConnectorType {
-		SPIGOT(new File("CentauriCloudSpigot.jar"), "https://centauricloud.net/downloads/latest/CentauriCloudSpigot.jar"), BUNGEE(new File("CentauriCloudBungee.jar"), "https://centauricloud.net/downloads/latest/CentauriCloudBungee.jar");
+		SPIGOT(new File(Cloud.getInstance().getSharedDir(), "CentauriCloudSpigot.jar"), "https://centauricloud.net/downloads/latest/CentauriCloudSpigot.jar"), BUNGEE(new File(Cloud.getInstance().getSharedDir(), "CentauriCloudBungee.jar"), "https://centauricloud.net/downloads/latest/CentauriCloudBungee.jar");
 
 		@Getter private final File file;
 		@Getter private final String remotePath;
